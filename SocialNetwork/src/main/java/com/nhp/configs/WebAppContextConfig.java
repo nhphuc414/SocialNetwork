acfb55885726cf664cfc0a005fc2a03e0c8e7a79
Nguyen Hong Phuc <nhphuc414@gmail.com>
2023-07-13 23:03:44 +0700

    init project

---
+++ SocialNetwork/src/main/java/com/nhp/configs/WebAppContextConfig.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nhp.configs;
+
+import java.text.SimpleDateFormat;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author ad
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.nhp.controllers",
+})
+public class WebAppContextConfig implements WebMvcConfigurer{
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver r = new InternalResourceViewResolver();
+        r.setViewClass(JstlView.class);
+        r.setPrefix("/WEB-INF/pages/");
+        r.setSuffix(".jsp");
+        
+        return r;
+    }
+    
+    @Bean
+    public SimpleDateFormat simpleDateFormat() {
+        return new SimpleDateFormat("yyyy-MM-dd");
+    }
+}
